2e9838ea97619367f50249b6cb9c9b9ecb4af8fc
al-mahfuz <almahfuzhossen@gmail.com>
2023-01-12 13:34:07 +0600

    js file added

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 const netPrice = document.getElementById('net-price');
-const netRate = document.getElementById('vat-rate');
+const vatRate = document.getElementById('vat-rate');
 const incAmount = document.getElementById('inc-amount');
 const calcBtn = document.getElementById('calc-btn');
 const resetBtn = document.getElementById('reset-btn');
 
 function VATInclusiveAmount(price, rate){
- let totalAmount = price + (price *(rate / 100));
- return totalAmount.toFixed(2);
+    let totalAmount = price + (price * (rate / 100));
+    return totalAmount.toFixed(2);
 }
 
 calcBtn.addEventListener('click', function(){
-    if (netPrice.value === "" || isNaN (netPrice.value)){
-        //show red border for invalid input
+    if(netPrice.value === "" || isNaN(netPrice.value)){
+        // showing red border for invalid input
         netPrice.style.border = "1px solid red";
-        // received red border after 1.5s
+
+        // removing red border after 1.5s
         setTimeout(function(){
             netPrice.style.border = "1px solid transparent";
         }, 1500);
+
         // clearing wrong input
         netPrice.value = "";
     }
 
-    if (netRate.value === "" || isNaN (netRate.value)){
-        //show red border for invalid input
-        netRate.style.border = "1px solid red";
-        // received red border after 1.5s
+    if(vatRate.value === "" || isNaN(vatRate.value)){
+        // showing red border for invalid input
+        vatRate.style.border = "1px solid red";
+
+        // removing red border after 1.5s
         setTimeout(function(){
-            netRate.style.border = "1px solid transparent";
+            vatRate.style.border = "1px solid transparent";
         }, 1500);
+
         // clearing wrong input
-        netRate.value = "";
+        vatRate.value = "";
     }
+
+    let finalAmount = VATInclusiveAmount(Number(netPrice.value), Number(vatRate.value));
+    incAmount.value = finalAmount;
+})
+
+resetBtn.addEventListener('click', function(){
+    netPrice.value = "";
+    vatRate.value = "";
+    incAmount.value = "";
 })
